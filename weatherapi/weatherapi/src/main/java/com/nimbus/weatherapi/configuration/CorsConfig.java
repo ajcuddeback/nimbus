13fa3171253aa5b43e12177663b271de2dae3e1f
ajcuddeback <ajcuddeback@gmail.com>
2025-07-15 17:11:29 -0400

    fix: Ensuring cors only runs for dev mode

--- weatherapi/weatherapi/src/main/java/com/nimbus/weatherapi/configuration/CorsConfig.java
+++ weatherapi/weatherapi/src/main/java/com/nimbus/weatherapi/configuration/CorsConfig.java
@@ -3,12 +3,14 @@ package com.nimbus.weatherapi.configuration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import java.util.ArrayList;
 
 @Configuration
+@Profile("dev")
 public class CorsConfig {
 
     @Value("${cors.allowed-origins}")
