c887e77b6b606bbb147763c56d9d668566b8a1d4
ajcuddeback <ajcuddeback@gmail.com>
2026-01-30 23:08:27 -0500

    feat: Adding home page and removing SSR

--- app/src/app/app.config.ts
+++ app/src/app/app.config.ts
@@ -1,24 +1,24 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { DatePipe } from '@angular/common';
 
 import { routes } from './app.routes';
-import {provideClientHydration, withEventReplay, withNoHttpTransferCache} from '@angular/platform-browser';
-import {provideHttpClient, withFetch} from '@angular/common/http';
-import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
-import {providePrimeNG} from 'primeng/config';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideClientHydration(withEventReplay()),
     provideAnimationsAsync(),
     providePrimeNG({
       theme: {
         preset: Aura
       }
     }),
-    provideHttpClient(withFetch())
+    provideHttpClient(withFetch()),
+    DatePipe
   ]
 };
